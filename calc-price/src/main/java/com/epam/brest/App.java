11fda106f145b99c9b77b7d165d05cdee0d0ace2
Mikhail <mishaklimovich@tut.by>
2020-01-29 19:00:51 +0100

    Update App.java

--- calc-price/src/main/java/com/epam/brest/App.java
+++ calc-price/src/main/java/com/epam/brest/App.java
@@ -2,6 +2,8 @@ package com.epam.brest;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        Integer a=15;
+        Integer b=15;
+        System.out.print("a="+a);
     }
 }
